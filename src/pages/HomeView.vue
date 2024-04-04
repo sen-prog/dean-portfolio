4ed716e4c23fc43ab731da3f93519b7ba94a4cbd
sen-prog <luigidinopol@gmail.com>
2024-04-05 00:29:53 +0800

    Edit App

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -81,10 +81,31 @@
         <v-col cols="12" class="padd" id="what-i-do">
           <div class="first" id="project">
             <v-row>
-  
-              <v-col cols="12" sm="6">
-                <div class="egg">
-                  <v-img src="../assets/person-icon-removed.png" max-height="280" width="auto"></v-img>
+              <v-col cols="12">
+
+                <div class="child">
+                  <v-btn icon="fas fa-laptop" color="#FBDF7E" class="text-white"></v-btn>
+                  <h3 class="ml-3 mt-4">Backend Programming</h3>
+                  <p class="text-grey ml-3 mt-4 text-caption">
+                    Laravel and Nodejs Progammer
+                  </p>
+                </div>
+
+                <div class="child">
+                  <v-btn icon="fas fa-user" color="#FBDF7E" class="text-white"></v-btn>
+                  <h3 class="ml-3 mt-4">Project Managing</h3>
+                  <p class="text-grey ml-3 mt-4 text-caption">
+                    Team Leader and 
+                    Documenation handler
+                  </p>
+                </div>
+
+                <div class="child">
+                  <v-btn icon="fas fa-laptop" color="#FBDF7E" class="text-white"></v-btn>
+                  <h3 class="ml-3 mt-4">Frontend Programming</h3>
+                  <p class="text-grey ml-3 mt-4 text-caption">
+                    Simple programming design
+                  </p>
                 </div>
               </v-col>
             </v-row>
@@ -392,79 +413,4 @@ export default defineComponent({
 }
 
   
-    .head{
-      position: relative;
-      text-align: center;
-      padding: 12px;
-      margin-bottom: 6px;
-      height: 400px;
-      width: 100%;
-      color: white;
-    }
-  
-    .head::before{
-      content: " ";
-      position: absolute;
-      top:0;
-      left: 0;
-      height: 100%;
-      width: 50%;
-      background: black;
-      transform: skew(0deg, 6deg);
-    }
-  
-    .head:after{
-      content: " ";
-      position: absolute;
-      top:0;
-      right: 0;
-      height: 100%;
-      width: 50%;
-      background: black;
-      transform: skew(0deg, -6deg);
-    }
-    .egg{
-      display: block;
-      margin-left: 100px;
-      margin-top: 50px;
-      width: 356px;
-      height: 300px;
-      background-color: #FBDF7E;
-      border-radius: 50% 50% 50% 50% / 60% 60% 40% 40%;
-    }
-  
-    .first{
-      width: 100%;
-      height: 280px;
-      text-align: center;
-      padding: 2rem 2rem;
-    }
-  
-    .child{
-      display: inline-block;
-      padding: 2rem 1rem;
-      vertical-align: middle;
-      text-align: center;
-      margin-right: 8px;
-    }
-  
-    .imgHover{
-      padding: 0 200px;
-    }
-  
-    .pre{
-      width: 100%;
-      height: 380px;
-      text-align: center;
-      padding: 0 200px;
-      background-color: #f5f5f5; 
-    }
-  
-    .hire{
-      width: 100%;
-      height: 200px;
-      padding: 0 200px;
-      background-color: #e9e9e9;
-      margin-top: -24px;
-    } 
-  </style>
+</style>
